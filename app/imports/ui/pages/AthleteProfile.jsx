25be9312a4cd4e2e4eed78f668edecb0303a1e8a
robert-lemon-uhm <rlemon@hawaii.edu>
2020-11-13 20:24:13 -1000

    Initial Athlete Profile Table

--- app/imports/ui/pages/AthleteProfile.jsx
+++ app/imports/ui/pages/AthleteProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Container, Header, Table } from 'semantic-ui-react';
 
 /** Renders the Page for adding a document. */
 class AthleteProfile extends React.Component {
@@ -7,10 +7,33 @@ class AthleteProfile extends React.Component {
   /** Render the Profile page */
   render() {
     return (
-        <div>
-          <Header as="h1">Athlete Profile Page</Header>
-          <Header as="h3">Assigned to Robert</Header>
-        </div>
+        <Container className="profile-page-container">
+          <Header as="h1" textAlign="center">
+            Firstname Lastname
+          </Header>
+          <Table singleLine unstackable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell width={3}>Sport</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Age</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Height</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Weight</Table.HeaderCell>
+                <Table.HeaderCell width={2}>Graduation</Table.HeaderCell>
+                <Table.HeaderCell width={4}>Major</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>Football</Table.Cell>
+                <Table.Cell>21</Table.Cell>
+                <Table.Cell>6&apos;1</Table.Cell>
+                <Table.Cell>200 lbs</Table.Cell>
+                <Table.Cell>2021</Table.Cell>
+                <Table.Cell>Communication</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+          </Table>
+        </Container>
     );
   }
 }
