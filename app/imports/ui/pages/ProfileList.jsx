79c76fc9a2b5dce9c7d92a3dd5e5a4ec052de80a
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2020-11-14 00:45:03 -1000

    Update ProfileList.jsx

--- app/imports/ui/pages/ProfileList.jsx
+++ app/imports/ui/pages/ProfileList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image, Table } from 'semantic-ui-react';
 
 /** Renders the Page for adding a document. */
 class ProfileList extends React.Component {
@@ -7,10 +7,65 @@ class ProfileList extends React.Component {
   /** Render the Profile page */
   render() {
     return (
-        <div>
-          <Header as="h1">Profile List Page</Header>
-          <Header as="h3">Assigned to Franz OR Andy</Header>
-        </div>
+        <Table basic='very' celled collapsing>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Employee</Table.HeaderCell>
+              <Table.HeaderCell>Correct Guesses</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Image src='https://react.semantic-ui.com/images/avatar/small/lena.png' rounded size='mini' />
+                  <Header.Content>
+                    Lena
+                    <Header.Subheader>Human Resources</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>22</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Image src='https://react.semantic-ui.com/images/avatar/small/matthew.png' rounded size='mini' />
+                  <Header.Content>
+                    Matthew
+                    <Header.Subheader>Fabric Design</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>15</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Image src='https://react.semantic-ui.com/images/avatar/small/lindsay.png' rounded size='mini' />
+                  <Header.Content>
+                    Lindsay
+                    <Header.Subheader>Entertainment</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>12</Table.Cell>
+            </Table.Row>
+            <Table.Row>
+              <Table.Cell>
+                <Header as='h4' image>
+                  <Image src='https://react.semantic-ui.com/images/avatar/small/mark.png' rounded size='mini' />
+                  <Header.Content>
+                    Mark
+                    <Header.Subheader>Executive</Header.Subheader>
+                  </Header.Content>
+                </Header>
+              </Table.Cell>
+              <Table.Cell>11</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
     );
   }
 }
