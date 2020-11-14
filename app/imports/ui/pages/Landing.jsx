d4d5e8c1889e817da8df754d9c1ff9cdedebbb01
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2020-11-13 21:37:19 -0800

    not finished with landing

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image } from 'semantic-ui-react';
 
 const FullImage = '../images/collage.png';
+
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
