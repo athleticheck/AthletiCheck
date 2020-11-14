151c63adfc12381a190cbe7a0f12f714bd99e5a1
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2020-11-14 01:14:03 -0800

    set negative margins

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Icon, Image } from 'semantic-ui-react';
 
 const backgroundImage = '../images/officialBackground.png';
+const menuStyle = { size: '205px 300px' };
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
@@ -9,20 +10,20 @@ class Landing extends React.Component {
         <div className='background-landing'>
           <Image src={backgroundImage} fluid/>
           <Grid verticalAlign='middle' textAlign='center'>
-            <Grid.Column width={4} id='landing-grid' color='red'>
+            <Grid.Column width={4} id='landing-grid' color='red' style={menuStyle}>
               <Icon name='users' size='huge' />
-              <h1>Contact Details</h1>
-              <h3>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h3>
+              <h1>Athlete Details</h1>
+              <h3>This platform allows a list of Athletes at your hands.</h3>
             </Grid.Column>
-            <Grid.Column width={4}>
+            <Grid.Column width={4} id='landing-grid' color='red' style={menuStyle}>
               <Icon name='file alternate' size='huge' />
-              <h1>Contact Details</h1>
-              <h3>For each contact, you can save their name, number, and phone number.</h3>
+              <h1>Communication</h1>
+              <h3>For each contact, you can save their sport, name, number, age, weight, height and major.</h3>
             </Grid.Column>
-            <Grid.Column width={4}>
+            <Grid.Column width={4} id='landing-grid' color='red' style={menuStyle}>
               <Icon name='calendar check' size='huge' />
               <h1>Timestamped Notes</h1>
-              <h3>Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.</h3>
+              <h3>Each time you visit with an Athlete, you can write notes to summarize the visit.</h3>
             </Grid.Column>
           </Grid>
         </div>
