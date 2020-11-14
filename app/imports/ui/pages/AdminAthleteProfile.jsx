b19400ff7ae9e6ede43a02468fe2a3984f2d66db
robert-lemon-uhm <rlemon@hawaii.edu>
2020-11-14 00:46:40 -1000

    Initial Commit
    NEW athlete profile page, info table is vertical instead of horizontal

--- app/imports/ui/pages/AdminAthleteProfile.jsx
+++ app/imports/ui/pages/AdminAthleteProfile.jsx
@@ -1,16 +1,66 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { Card, Container, Grid, Header, Image, Table, Button, Divider } from 'semantic-ui-react';
 
 /** Renders the Page for adding a document. */
 class AdminAthleteProfile extends React.Component {
 
-  /** Render the Profile page */
+  /** Render the Admin Profile page */
   render() {
     return (
-        <div>
-          <Header as="h1">Admin Athlete Profile Page</Header>
-          <Header as="h3">Assigned to Robert</Header>
-        </div>
+        <Container className='profile-page-container'>
+          <Card fluid centered>
+            <Card.Content textAlign='center'>
+              <Grid columns={2} stackable divided>
+                <Grid.Column>
+                  <Image centered size='medium' rounded
+                         src="https://www.ics.hawaii.edu/wp-content/uploads/2018/08/peter-sadowski_300x300.jpg"/>
+                </Grid.Column>
+                <Grid.Column>
+                  <Header as='h1' textAlign='center' attached='top' inverted>
+                    Peter Sadowski
+                  </Header>
+                  <Table singleLine stackable inverted attached>
+                    <Table.Body>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Sport</Table.HeaderCell>
+                        <Table.Cell>Football</Table.Cell>
+                      </Table.Row>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Age</Table.HeaderCell>
+                        <Table.Cell>21</Table.Cell>
+                      </Table.Row>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Height</Table.HeaderCell>
+                        <Table.Cell>6&apos;5</Table.Cell>
+                      </Table.Row>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Weight</Table.HeaderCell>
+                        <Table.Cell>200 lbs</Table.Cell>
+                      </Table.Row>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Graduation</Table.HeaderCell>
+                        <Table.Cell>2022</Table.Cell>
+                      </Table.Row>
+                      <Table.Row textAlign="center">
+                        <Table.HeaderCell>Major</Table.HeaderCell>
+                        <Table.Cell>Communications</Table.Cell>
+                      </Table.Row>
+                    </Table.Body>
+                  </Table>
+                </Grid.Column>
+              </Grid>
+            </Card.Content>
+            <Card.Content extra>
+              <Button fluid>
+                Edit Profile
+              </Button>
+            </Card.Content>
+          </Card>
+          <Divider horizontal>
+            <Header as='h1' textAlign='center' inverted>VISITS</Header>
+          </Divider>
+          No visits! (make a &quot;no visits&quot; comment/card by default)
+        </Container>
     );
   }
 }
