97a278a5c49ca39c09660ca4574a8f4ae50bdbb2
robert-lemon-uhm <rlemon@hawaii.edu>
2020-11-13 21:48:52 -1000

    Edit
    Made Cards for profiles, added profile image

--- app/imports/ui/pages/AthleteProfile.jsx
+++ app/imports/ui/pages/AthleteProfile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Header, Table } from 'semantic-ui-react';
+import { Container, Table, Card, Image, Header } from 'semantic-ui-react';
 
 /** Renders the Page for adding a document. */
 class AthleteProfile extends React.Component {
@@ -7,32 +7,38 @@ class AthleteProfile extends React.Component {
   /** Render the Profile page */
   render() {
     return (
-        <Container className="profile-page-container">
-          <Header as="h1" textAlign="center" inverted>
-            Firstname Lastname
-          </Header>
-          <Table singleLine unstackable>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell width={3}>Sport</Table.HeaderCell>
-                <Table.HeaderCell width={2}>Age</Table.HeaderCell>
-                <Table.HeaderCell width={2}>Height</Table.HeaderCell>
-                <Table.HeaderCell width={2}>Weight</Table.HeaderCell>
-                <Table.HeaderCell width={2}>Graduation</Table.HeaderCell>
-                <Table.HeaderCell width={4}>Major</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>Football</Table.Cell>
-                <Table.Cell>21</Table.Cell>
-                <Table.Cell>6&apos;1</Table.Cell>
-                <Table.Cell>200 lbs</Table.Cell>
-                <Table.Cell>2021</Table.Cell>
-                <Table.Cell>Communication</Table.Cell>
-              </Table.Row>
-            </Table.Body>
-          </Table>
+        <Container className='profile-page-container'>
+          <Card fluid centered>
+            <Card.Content textAlign='center'>
+              <Image centered size='medium' circular
+                     src="https://www.ics.hawaii.edu/wp-content/uploads/2018/08/peter-sadowski_300x300.jpg"/>
+            </Card.Content>
+            <Card.Content extra>
+              <Header as='h1' textAlign='center'>Peter Sadowski</Header>
+              <Table singleLine unstackable inverted>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell width={3}>Sport</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Age</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Height</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Weight</Table.HeaderCell>
+                    <Table.HeaderCell width={2}>Graduation</Table.HeaderCell>
+                    <Table.HeaderCell width={4}>Major</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell>Football</Table.Cell>
+                    <Table.Cell>21</Table.Cell>
+                    <Table.Cell>6&apos;5</Table.Cell>
+                    <Table.Cell>200 lbs</Table.Cell>
+                    <Table.Cell>2021</Table.Cell>
+                    <Table.Cell>Communication</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Card.Content>
+          </Card>
         </Container>
     );
   }
