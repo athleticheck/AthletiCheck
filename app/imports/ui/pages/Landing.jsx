292913f54723c4a3e8a38b32c800138f4d5d6c2f
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2020-11-14 00:39:54 -0800

    negative margin

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Grid, Icon } from 'semantic-ui-react';
+import { Grid, Icon, Image } from 'semantic-ui-react';
 
-// const backgroundImage = '../images/officialBackground.png';
+const backgroundImage = '../images/officialBackground.png';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <div className='background-landing'>
-          <Grid verticalAlign='middle' textAlign='center' container>
-            <Grid.Column width={4}>
+          <Image src={backgroundImage} fluid/>
+          <Grid verticalAlign='middle' textAlign='center'>
+            <Grid.Column width={4} id='landing-grid' color='red'>
               <Icon name='users' size='huge' />
               <h1>Contact Details</h1>
               <h3>This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.</h3>
