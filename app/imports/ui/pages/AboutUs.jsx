a82be85a243e8ea9d74dd5533f70f629fdd79316
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2020-11-28 14:05:58 -0800

    Issue #18: Added to the about us page

--- app/imports/ui/pages/AboutUs.jsx
+++ app/imports/ui/pages/AboutUs.jsx
@@ -1,48 +1,41 @@
 import React from 'react';
-import { Header, Image, Grid, Container, Segment } from 'semantic-ui-react';
+import { Header, Image, Grid, Container } from 'semantic-ui-react';
 
+const bioStyle = { width: '20%' };
 export default class AboutUs extends React.Component {
 
   render() {
     const divStyle = {
-      paddingTop: '15px',
+      paddingTop: '10px',
       color: 'white',
     };
     return (
         <Container>
-            <Header as='h1' style={divStyle} textAlign="center" size="huge" className='about-welcome'> WELCOME </Header>
-            <Header as='h2' textAlign="center" size="medium">We are AthletiCheck, providing you with the most efficient way to manage Athletes.</Header>
-          <Grid>
-            <Grid.Column className="about-description">
-              <Segment inverted>Inverted content.
-              <Header>second half thats below</Header>
-              </Segment>
-            </Grid.Column>
-          </Grid>
-          <div className="aboutus-bios">
-          <Grid colums={2}>
-            <Grid.Row>
-            <Grid.Column>
-              <Image src='https://franzadam.github.io/images/MeTi.jpg' />
-              <Header>FRANZ ADAMS</Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Image src='https://robert-lemon-uhm.github.io/images/SquarePic.png' />
-              <Header>ROBERT LEMON</Header>
-            </Grid.Column>
-            </Grid.Row>
-            <Grid.Row>
-              <Grid.Column>
-                <Image src='https://andyyu824.github.io/images/andyyu.jpg' />
-                <Header>ANDY YU</Header>
-              </Grid.Column>
-              <Grid.Column>
-                <Image src='https://annacampainha.github.io/images/annapic.png' />
-                <Header>ANNA CAMPAINHA</Header>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-          </div>
+            <Header as='h1' style={divStyle} textAlign="center" size="huge" className='about-welcome' > WELCOME </Header>
+            <hr />
+            <Header as='h2' style={divStyle} textAlign="center" size="medium">We are AthletiCheck, providing you with the most efficient way to manage Athletes.</Header>
+          <div className="about-us-container">
+            <Header as='h1' textAlign="center" size="huge" className='about-welcome' > <u> OUR TEAM </u> </Header>
+              <Grid columns={4} centered>
+                  <Grid.Column style={bioStyle}>
+                    <Image src='../images/franz.jpg' size='medium' circular/>
+                    <Header textAlign='center'>FRANZ ADAMS</Header>
+                    <p textalign="center">franzadam@gmail.com</p>
+                  </Grid.Column>
+                  <Grid.Column style={bioStyle}>
+                    <Image src='https://robert-lemon-uhm.github.io/images/SquarePic.png' size='medium' circular/>
+                    <Header textAlign='center'>ROBERT LEMON</Header>
+                  </Grid.Column>
+                  <Grid.Column style={bioStyle}>
+                    <Image src='https://andyyu824.github.io/images/andyyu.jpg' size='medium' circular/>
+                    <Header textAlign='center'>ANDY YU</Header>
+                  </Grid.Column>
+                  <Grid.Column style={bioStyle}>
+                    <Image src='https://annacampainha.github.io/images/annapic.png' size='medium' circular/>
+                    <Header textAlign='center'>ANNA CAMPAINHA</Header>
+                  </Grid.Column>
+              </Grid>
+            </div>
         </Container>
     );
   }
