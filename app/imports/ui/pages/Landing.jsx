09fa1f090ba58cda45c0cdc5e16a0d08c5194f46
robert-lemon-uhm <rlemon@hawaii.edu>
2020-11-27 14:38:03 -1000

    Revert "fixed button"
    This reverts commit bf2be99c697cde88e8a3e535d76354f5ae24f695.

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -36,7 +36,7 @@ class Landing extends React.Component {
               <Grid.Column className="landing-image">
                 <Image src='/images/signup.png' fluid/>
                 <Label size='massive' style={buttonStyle} color='black'>
-                <Button style={buttonStyle} size='small' inverted color='red'
+                <Button style={buttonStyle} size='small' color='grey'
                         as={NavLink} activeClassName="active"exact to="/about">
                   <Button.Content>
                     ABOUT US
