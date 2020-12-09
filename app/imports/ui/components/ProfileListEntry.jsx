e3741db2a92fa215fa0ce3b656e00e629610b57d
Andy Yu <67715364+andyyu824@users.noreply.github.com>
2020-12-09 02:39:09 -1000

    Button doesn't work, console errors

--- app/imports/ui/components/ProfileListEntry.jsx
+++ app/imports/ui/components/ProfileListEntry.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Table } from 'semantic-ui-react';
+import { Button, Table, Image } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
