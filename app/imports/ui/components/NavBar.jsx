2732765c6014db09b8d3886506e56b806c971b7d
Anna Campainha <70426898+annacampainha@users.noreply.github.com>
2020-12-11 12:53:19 -0800

    Issue #30: COVID form opens in a new

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
         </Menu.Item>
       {this.props.currentUser && !Roles.userIsInRole(Meteor.userId(), 'admin') ? ([<Menu.Item as={NavLink} activeClassName="active" exact to="/profile" key='profile'>My Profile</Menu.Item>,
             <Popup key='popup' content='DO NOT FILL THIS FORM OUT. IT IS THE REAL UH COVID FORM.'
-            trigger={ <Menu.Item as='a' href='https://docs.google.com/forms/d/e/1FAIpQLSdeC4vggGnSE4inATxnOSFc41FsDpuk42gcblPQ17AW_86FOw/viewform?vc=0&c=0&w=1'
+            trigger={ <Menu.Item as='a' target="_blank" href='https://docs.google.com/forms/d/e/1FAIpQLSdeC4vggGnSE4inATxnOSFc41FsDpuk42gcblPQ17AW_86FOw/viewform?vc=0&c=0&w=1'
                                  key='googleform'>COVID Form</Menu.Item>} />]
         ) : ''}
         {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
